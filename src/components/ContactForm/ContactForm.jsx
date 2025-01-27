ef3d5768e48918315242ecb0305a1eac98a85fe0
Oleh Skrypets <olehskrupets@gmail.com>
2025-01-27 21:03:17 +0200

    fix import

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,7 +2,7 @@ import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as Yup from "yup";
 import styles from "./ContactForm.module.css";
 import { useDispatch } from "react-redux";
-import { addContact } from "../../redux/ContactsOps";
+import { addContact } from "../../redux/contactsOps";
 
 const validationSchema = Yup.object({
   name: Yup.string()
